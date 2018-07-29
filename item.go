b9cf9b4971d63a093a25291f1058063ce08e5c02
Shawn Smith <shawnsmithdev@users.noreply.github.com>
2018-07-28 21:40:27 -0700

    Drop VersionedItemMap and unused code

--- item.go
+++ item.go
@@ -2,10 +2,8 @@ package ddbmap
 
 import (
 	ddb "github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/dynamodbattribute"
-	"github.com/shawnsmithdev/ddbmap/ddbconv"
-	"log"
-)
+		"github.com/shawnsmithdev/ddbmap/ddbconv"
+	)
 
 // Item is a type alias for map[string]AttributeValue, the output of dynamodbattribute.MarshalMap.
 // This represents a single row in a DynamoDB table or a 'Map' in the DynamoDB type system.
@@ -14,20 +12,6 @@ type Item map[string]ddb.AttributeValue
 // AsItem directly returns this item.
 func (item Item) AsItem() Item {
 	return item
-}
-
-// MarshalItem calls dynamodbattribute.MarshalMap on its input and returns the resulting Item.
-// It is different in that it does not return an error, but will panic if given value cannot be marshaled.
-func MarshalItem(x interface{}) Item {
-	item, err := marshalItem(x)
-	forbidErr(err, log.Println)
-	return item
-}
-
-// UnmarshalItem calls dynamodbattribute.UnmarshalMap with given input item and output pointer.
-// It is different in that it does not return an error, but will panic if given value cannot be unmarshaled.
-func (item Item) Unmarshal(out interface{}) {
-	forbidErr(dynamodbattribute.UnmarshalMap(item, out), log.Println)
 }
 
 // GetAsBinary returns the attribute of this item with the given name as a byte slice,
@@ -121,15 +105,12 @@ type ItemMap interface {
 	// If the consumer returns false, range eventually stops the iteration.
 	// If a consumer returns false once, it should eventually always return false.
 	RangeItems(consumer func(Item) (resume bool)) error
-}
 
-// VersionedItemMap is an ItemMap that includes some compare-and-swap methods using a configured int64 version field.
-type VersionedItemMap interface {
-	ItemMap
 	// StoreIfVersion stores the given item if there is an existing item with the same key(s) and the given version.
 	// Returns true if the item was stored.
 	StoreIfVersion(val interface{}, version int64) (ok bool)
 	// StoreItemIfVersion stores the given item if there is an existing item with the same key(s) and the given version.
 	// Returns true if the item was stored.
-	StoreItemIfVersion(item Itemable, version int64) (ok bool)
+	StoreItemIfVersion(item Itemable, version int64) (ok bool, err error)
 }
+
